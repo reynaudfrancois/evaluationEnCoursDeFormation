306acc1827c583fe8aa1f7a58beecf29e3564f99
reynaudfrancois <freynaut@laposte.net>
2023-05-20 16:00:15 +0200

    Creation a a new file index.php
    Creation of a nex file "index.php" ; creation of a new folder "annexe" to stock the older files index.php and upload.php, which serve to upload images.

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>upload with php</title>
+	<title>Quai Antique</title>
 </head>
 
 <body>
 
-	<form action="upload.php" method="post" enctype="multipart/form-data">
-		
-		<input type="file" name="fileImg" id="fileImg">
-		<input type="submit" name="sub" value="upload">
-
-	</form>
+	<h1>Quai Antique</h1>
 
 </body>
 </html>
