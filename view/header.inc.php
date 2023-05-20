1b671cfd5e50a687c6665600398755e0fb2c09c5
reynaudfrancois <freynaut@laposte.net>
2023-05-20 18:43:53 +0200

    Change attributes href
    Change relative path in absolute path

--- view/header.inc.php
+++ view/header.inc.php
@@ -16,8 +16,8 @@
 		<h1>Quai Antique</h1>
 
 		<nav>
-			<a href='index.php' class='nav'>Accueil</a>
-			<a href='view/card.php' class='nav'>Carte</a>
+			<a href='/evaluationEnCoursDeFormation/index.php' class='nav'>Accueil</a>
+			<a href='/evaluationEnCoursDeFormation/view/card.php' class='nav'>Carte</a>
 			<a href='' class='nav'>Menus</a>
 			<a href='' class='nav'>Connexion</a><br/>
 			<a href='' id='reserve'>RESERVER</a>
