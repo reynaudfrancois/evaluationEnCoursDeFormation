f4ede63e89e464c6336cc61af918c7ef03ffc912
reynaudfrancois <freynaut@laposte.net>
2023-05-21 17:33:43 +0200

    Creation of the page of the menu
    Creation of the page of the menu

--- view/card.php
+++ view/card.php
@@ -5,13 +5,35 @@
 		<h2>NOTRE CARTE</h2>
 
 		<section>
-			
-			<h2>LOREM IPSUM</h2>
-
-			<h3>Lorem ipsum - XX €</h3>
-
+			<h3>ENTREES</h3>
+			<h4>Lorem ipsum - XX €</h4>
 			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+		</section>
 
+		<section>
+			<h3>BURGERS</h3>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+		</section>
+
+		<section>
+			<h3>PLATS</h3>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
 		</section>
 
 	</main>
---
+++ view/menu.php
@@ -0,0 +1,32 @@
+<?php require "header.inc.php"; ?> 
+
+	<main id="menu">
+
+		<h2>NOS MENUS</h2>
+
+		<section>
+			<h3>MENU 1</h3>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+		</section>
+
+		<section>
+			<h3>MENU 2</h3>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+			<h4>Lorem ipsum - XX €</h4>
+			<p>His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos</p>
+		</section>
+		
+	</main>
+
+<?php require "footer.inc.php"; ?> 
+
+
+
